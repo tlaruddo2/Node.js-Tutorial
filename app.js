f86be320034f3979b9121b848cd6946807596566
Kukjin Kim <tlaruddo2@gmail.com>
2022-01-26 02:47:11 -0800

    Node Module System-Path, OS, File system Module

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-const logger = require('./logger');
+//built in module in core of Node (check in website)
+
+//not built in need to path  ('./moduleName')
+//built int module  ('modulename')
+
+
+
+//path module
+const path = require('path')
+
+//parse method: give us infor about module
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+
+
+//OS module 
+//how to get info about current OS 
+const os = require('os'); 
+var totalMemory = os.totalmem()
+var freeMemory = os.freemem();
+
+console.log ('Ootal memory: ' + totalMemory);
+
+//Template String (in ES6 or ES2015 : ECMASCript 6)
+//no need concat
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Total Memory: ${freeMemory}`);
+
+//if JS working on browser, we could not get this info 
+//These info is avaliable on OS and server
+
+
+//File system Module 
+//Working file and directory
+//this have syncronized and asysncronize method
+// use asysncronize moethod (non-blocking)
+const fs = require('fs');
+
+//return all file and folder in current folder 
+const files = fs.readdirSync('./'); // current folder 
+console.log(files);
+
+//all asuncronized function 
+//last argument is call back function 
+//files == string array
+fs.readdir('./', function(err, files){
+    if (err) console.log("error", err);
+    else console.log('result',files);
+});
--- logger.js
+++ logger.js
@@ -1,29 +1,11 @@
-(funtion (exports, require, module, __filename, __dirname){
-    
-    var url = 'httpL//mylogger.io/log';
+var url = 'httpL//mylogger.io/log';
 
-    function log(message){
-        //assume send an HTTP request
-        console.log(message);
-    }
+function log(message){
+    //assume send an HTTP request
+    console.log(message);
+}
 
-    module.exports = log;
-
-    // module.exports.log = log; possible
-    // exports.log = log;possible
-
-    // exports = log; // not able 
-    // since it is reference of exprots module (module.exprots)
+module.exports = log;
 
 
-})
 
-//how module work: Module Wrapper function 
-
-//node does not excute this code directly
-//wrap inside of a fuction 
-// (funtion (exports, require, module, __filename, __dirname){
-
-//require not globall
-
-//__dirname:path of directory
