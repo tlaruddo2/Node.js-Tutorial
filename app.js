f15c72e0924c5a04f42c8eebf94f7840764fe14d
Kukjin Kim <tlaruddo2@gmail.com>
2022-01-26 01:47:22 -0800

    Node Module System-Global Object

---
+++ app.js
@@ -0,0 +1,21 @@
+//global object in JS
+console.log();; 
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+//in browser
+//all variable function defined globally accessed via window obeject
+window.console.log();
+var message = '';
+window.message;
+
+//in node , no window object
+//instead global object (access function but not variable)
+global.console.log();
+var message2 = '';
+console.log(global.message2); // undefined
+
